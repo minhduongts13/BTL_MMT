5e7bcfe256f210a8d168b9cc92737d6b1265b274
minhduongts13 <156182694+minhduongts13@users.noreply.github.com>
2024-11-12 22:50:09 +0700

    10h50 11/12/2024

--- Peer/file_manager.py
+++ Peer/file_manager.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import hashlib
 
 def split_file(file_path, piece_size=512*1024):
@@ -26,33 +25,4 @@ def generate_piece_hashes(pieces):
 
 def verify_piece(piece, expected_hash):
     piece_hash = hashlib.sha256(piece).hexdigest()
-=======
-import hashlib
-
-def split_file(file_path, piece_size=512*1024):
-    pieces = []
-    with open(file_path, 'rb') as file:
-        while True:
-            piece = file.read(piece_size)
-            if not piece:
-                break
-            pieces.append(piece)
-    return pieces
-
-def assemble_file(pieces, output_file):
-    with open(output_file, 'wb') as file:
-        for piece in pieces:
-            file.write(piece)
-    print(f"File assembled: {output_file}")
-
-def generate_piece_hashes(pieces):
-    piece_hashes = []
-    for piece in pieces:
-        piece_hash = hashlib.sha256(piece).hexdigest()
-        piece_hashes.append(piece_hash)
-    return piece_hashes
-
-def verify_piece(piece, expected_hash):
-    piece_hash = hashlib.sha256(piece).hexdigest()
->>>>>>> c3d6106 (Initial commit)
     return piece_hash == expected_hash
